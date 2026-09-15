36bb19e1943b89100ec2b1c2238d48f5136b863b
Anushka Pateriya <anushkapateriya05@gmail.com>
2026-09-15 23:29:32 +0530

    login page frontend

---
+++ frontend/login.js
@@ -0,0 +1,24 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async function(event) {
+
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+        const response = await axios.post("http://localhost:3000/login", {
+            email: email,
+            password: password
+        });
+
+        document.getElementById("message").innerText = response.data.message;
+
+    } catch (error) {
+
+        document.getElementById("message").innerText = error.response.data.message;
+
+    }
+});
